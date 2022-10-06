5ef60207e8068d9f0b0976b7d428c239c5c0fb42
locphuong-primelabo <loc.phuong@primelabo.com.vn>
2022-10-06 16:18:16 +0700

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Console;
 
-// tesst  122
+// tesst  12245
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
